2863f0859e327646c574fb59c0593c62ca6222a6
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-09-24 13:03:55 -0300

    style: taskform

--- src/components/todolist/TaskForm.jsx
+++ src/components/todolist/TaskForm.jsx
@@ -1,4 +1,42 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 8px;
+  margin: 10px;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 8px;
+`;
+
+const ContentInputs = styled.div`
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+`;
+
+const Input = styled.input`
+  padding: 8px;
+  border-radius: 4px;
+  border: none;
+  margin-right: 4px;
+  width: 300px;
+  outline-color: #2384e6f5;
+`;
+
+const Select = styled.select`
+  padding: 8px;
+  border-radius: 4px;
+  border: none;
+  margin-right: 4px;
+  outline-color: #2384e6f5;
+`;
 
 const TaskForm = ({ addTask }) => {
   const [text, setText] = useState('');
@@ -12,27 +50,47 @@ const TaskForm = ({ addTask }) => {
     setCategory('');
   };
 
+  const Button = styled.button`
+    padding: 10px 15px;
+    border-radius: 4px;
+    border: none;
+    background-color: #333974;
+    color: #fff;
+    cursor: pointer;
+    transition: 0.3s;
+    opacity: 0.8;
+
+    &:hover {
+      opacity: 1;
+    }
+  `;
+
   return (
-    <div>
+    <Content>
       <form onSubmit={handleSubmit}>
-        <h2>Adicionar tarefa</h2>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Digite a tarefa"
-        />
+        <Title>Adicionar tarefa</Title>
+        <ContentInputs>
+          <Input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Digite a tarefa"
+          />
 
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="">Selecione a categoria</option>
-          <option value="Estudo">Estudo</option>
-          <option value="Pessoal">Pessoal</option>
-          <option value="Trabalho">Trabalho</option>
-        </select>
+          <Select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">Selecione a categoria</option>
+            <option value="Estudo">Estudo</option>
+            <option value="Pessoal">Pessoal</option>
+            <option value="Trabalho">Trabalho</option>
+          </Select>
 
-        <button type="submit">Adicionar Tarefa</button>
+          <Button type="submit">Adicionar Tarefa</Button>
+        </ContentInputs>
       </form>
-    </div>
+    </Content>
   );
 };
 
