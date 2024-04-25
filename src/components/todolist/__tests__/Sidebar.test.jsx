005c9546ea2eef854ba2f04d47749e2632e3da87
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2024-04-25 16:21:27 -0300

    test: added test sidebar

---
+++ src/components/todolist/__tests__/Sidebar.test.jsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+import Sidebar from '../Sidebar';
+import { UserContext } from '../../../UserContext';
+
+const renderSidebarWithContext = () => {
+  const defaultValues = {
+    search: '',
+    setSearch: jest.fn(),
+  };
+
+  return render(
+    <UserContext.Provider value={defaultValues}>
+      <Sidebar />
+    </UserContext.Provider>,
+  );
+};
+
+describe('Test Sidebar', () => {
+  it.only('should render the Search Component in the sidebar', () => {
+    const { getByText } = renderSidebarWithContext();
+
+    const titleSearch = getByText('Pesquisar');
+
+    expect(titleSearch).toBeInTheDocument();
+  });
+
+  it.only('should render the Filter Component in the sidebar', () => {
+    const { getByText } = renderSidebarWithContext();
+
+    const title = getByText('Filtros');
+
+    expect(title).toBeInTheDocument();
+  });
+});
